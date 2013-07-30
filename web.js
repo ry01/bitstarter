d546109fcc0cfcfd547b5563d7ed8ddbc00e53d4
Ubuntu <ubuntu@ip-172-31-47-41.us-west-2.compute.internal>
2013-07-30 06:12:12 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+// Read in the contents of the html file.
+// The function fs.readFile returns a buffer unless an encoding is specified.
+var buf = fs.readFile("index.html");
+
+//
+// Convert that buffer to a string.
+var string = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
