2dfe96be4b00a6ba992dccb0a66905481a65c555
Ubuntu <ubuntu@ip-172-31-47-41.us-west-2.compute.internal>
2013-07-30 06:44:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,14 +6,14 @@ app.use(express.logger());
 // Read in the contents of the html file.
 // The function fs.readFile returns a buffer unless an encoding is specified.
 //var buf = fs.readFileSync('index.html');
-//var text = fs.readFileSync('index.html','utf8')
+var text = fs.readFileSync('index.html','utf8')
 
 //
 // Convert that buffer to a string.
 //var text = buf.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hi!');
+  response.send('Hi Me!');
 });
 
 var port = process.env.PORT || 5000;
